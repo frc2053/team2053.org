ff411f8b2b280a3f90dc182d44eecbb1868da3b7
Drew Williams <williams.r.drew@gmail.com>
2022-12-07 15:52:45 -0500

    deleted unused images

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -38,24 +38,7 @@ export const footerSocials = [
 
 export const footerLists = [
 	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-			// {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
-	{
-		title: 'Company',
+		title: 'Our Team',
 		items: [
 			{
 				title: 'About',
@@ -70,26 +53,5 @@ export const footerLists = [
 				slug: '/company/contact',
 			},
 		],
-	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-			{
-				title: 'Theme Setup',
-				slug: '/theme/theme-setup',
-			},
-			{
-				title: 'Customizing Odyssey',
-				slug: '/theme/customizing-odyssey',
-			}
-		],
-	},
+	}
 ];
