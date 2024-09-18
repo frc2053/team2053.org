2b47786d2a53994b3743bc67d61aba85625638d7
Drew Williams <williams.r.drew@gmail.com>
2024-09-18 13:14:27 -0400

    upgrade to latest odyssey template and astro 4

---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Southern Tier Robotics | FIRST Robotics Competition Team 2053`,
+	description: `FRC 2053's website dedicated to making information about our team easier to find`,
+	url: `https://team2053.org`, // No trailing slash!
+	name: `STR`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
