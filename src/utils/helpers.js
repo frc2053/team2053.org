c776d9ee9d34bd161b09d2d6d3d36e7e57a83c79
Drew Williams <DrewW@iARx.com>
2025-08-07 10:04:57 -0400

    reorganized blog so you dont need funky file names

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,13 +10,36 @@ export function generateSlug(string) {
 		.replace(/-+$/, '');
 }
 
-export function generateTagData(categories) {
-	let categoryData = [];
-	categories.forEach(category => {
-		categoryData.push({
-			title: category,
-			slug: `${generateSlug(category)}`,
-		});
-	});
-	return categoryData;
+export function getAllTags(posts) {
+    const allTags = posts
+        .flatMap((post) => post.data.tags || [])
+        .filter(Boolean);
+    
+    const uniqueTags = [...new Set(allTags)];
+    
+    return uniqueTags.map((tag) => ({
+        title: tag,
+        slug: tag.toLowerCase().replace(/\s+/g, '-'),
+    }));
+}
+
+export function getPostsByTag(posts, tagName) {
+    return posts
+        .filter((post) => post.data.tags && post.data.tags.includes(tagName))
+        .map((post) => ({
+            title: post.data.title,
+            description: post.data.description,
+            publishDate: post.data.publishDate,
+            featuredImage: post.data.featuredImage,
+            excerpt: post.data.excerpt,
+            href: `/blog/${post.slug}`,
+        }))
+        .sort((a, b) => b.publishDate.getTime() - a.publishDate.getTime());
+}
+
+export function generateTagData(tags) {
+    return tags.map((tag) => ({
+        title: tag,
+        slug: tag.toLowerCase().replace(/\s+/g, '-'),
+    }));
 }
